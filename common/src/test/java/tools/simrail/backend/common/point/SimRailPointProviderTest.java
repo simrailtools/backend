2e5611f930f9d9b4fd96f495ed196656a3d9c854
Pasqual Koschmieder <git@derklaro.dev>
2024-12-14 03:14:22 +0100

    add two missing points with dispatch post

--- common/src/test/java/tools/simrail/backend/common/point/SimRailPointProviderTest.java
+++ common/src/test/java/tools/simrail/backend/common/point/SimRailPointProviderTest.java
@@ -53,7 +53,7 @@ public final class SimRailPointProviderTest {
   @Test
   void testPointsWereLoaded() {
     var points = this.pointProvider.points;
-    Assertions.assertEquals(534, points.size());
+    Assertions.assertEquals(536, points.size());
   }
 
   @Test
