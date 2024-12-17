68b8f3390993d2eadb78f5f02540b299f8bef296
Pasqual Koschmieder <git@derklaro.dev>
2024-12-17 12:08:46 +0100

    remove EnableWebMvc annotation from WebMvcConfiguration

--- rest-api/src/main/java/tools/simrail/backend/api/configuration/WebMvcConfiguration.java
+++ rest-api/src/main/java/tools/simrail/backend/api/configuration/WebMvcConfiguration.java
@@ -29,13 +29,11 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tools.simrail.backend.api.util.ETagGenerator;
 
-@EnableWebMvc
 @Configuration
 class WebMvcConfiguration implements WebMvcConfigurer {
 
