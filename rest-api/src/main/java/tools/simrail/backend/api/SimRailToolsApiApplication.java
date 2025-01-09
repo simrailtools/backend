b903ea088ce57958f216c30d608b61d52d0336f3
Pasqual Koschmieder <git@derklaro.dev>
2025-01-10 00:12:11 +0100

    update copyright for 2025

--- rest-api/src/main/java/tools/simrail/backend/api/SimRailToolsApiApplication.java
+++ rest-api/src/main/java/tools/simrail/backend/api/SimRailToolsApiApplication.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of simrail-tools-backend, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2024 Pasqual Koschmieder and contributors
+ * Copyright (c) 2024-2025 Pasqual Koschmieder and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
