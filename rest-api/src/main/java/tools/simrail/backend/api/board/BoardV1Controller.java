013eb8b86a9664f8f31901d9af279001d0d29fd7
Pasqual Koschmieder <git@derklaro.dev>
2025-04-19 20:05:49 +0200

    finish docs

--- rest-api/src/main/java/tools/simrail/backend/api/board/BoardV1Controller.java
+++ rest-api/src/main/java/tools/simrail/backend/api/board/BoardV1Controller.java
@@ -24,6 +24,11 @@
 
 package tools.simrail.backend.api.board;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Nonnull;
 import java.time.OffsetDateTime;
@@ -55,7 +60,40 @@ class BoardV1Controller {
     this.boardService = boardService;
   }
 
+  /**
+   * Lists all arrivals that are matching the given requests parameters.
+   */
   @GetMapping("/arrivals")
+  @Operation(
+    summary = "Get all arrivals within a specified timespan at a specified point",
+    description = """
+      Lists all journeys that are arriving at the specified point on the specified server within the given time span.
+      By default, a time span of 30 minutes is returned, starting at the current server time, unless otherwise specified.
+      Results are sorted by realtime time information by default. A minimum time span of 5 minutes and a maximum time
+      span of 6 hours can be requested. The start time can be 1 day in the future and 90 days in the past.
+      """,
+    parameters = {
+      @Parameter(name = "serverId", description = "The id of the server to get the board on"),
+      @Parameter(name = "pointId", description = "The id of the point to get the board of"),
+      @Parameter(name = "timeStart", description = "The start time for the board, if omitted current server time will be used"),
+      @Parameter(name = "timeEnd", description = "The end time for the board, if omitted the start time plus 30 minutes will be used"),
+      @Parameter(name = "transportTypes", description = "Filter for transports that should be returned, if omitted all transports will be returned"),
+      @Parameter(name = "sortBy", description = "Sort order of board entries, of omitted entries will be sorted by realtime time information"),
+    },
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "The arrival board entries were successfully resolved"),
+      @ApiResponse(
+        responseCode = "400",
+        description = "One of the given filter parameters is invalid or doesn't comply to the described constraints",
+        content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(
+        responseCode = "500",
+        description = "An internal error occurred while processing the request",
+        content = @Content(schema = @Schema(hidden = true))),
+    }
+  )
   public @Nonnull List<BoardEntryDto> listArrivals(
     @RequestParam(name = "serverId") @UUID(version = 5, allowNil = false) String serverId,
     @RequestParam(name = "pointId") @UUID(version = 4, allowNil = false) String pointId,
@@ -72,7 +110,40 @@ class BoardV1Controller {
     return arrivals;
   }
 
+  /**
+   * Lists all departures that are matching the given requests parameters.
+   */
   @GetMapping("/departures")
+  @Operation(
+    summary = "Get all departures within a specified timespan at a specified point",
+    description = """
+      Lists all journeys that are departing from the specified point on the specified server within the given time span.
+      By default, a time span of 30 minutes is returned, starting at the current server time, unless otherwise specified.
+      Results are sorted by realtime time information by default. A minimum time span of 5 minutes and a maximum time
+      span of 6 hours can be requested. The start time can be 1 day in the future and 90 days in the past.
+      """,
+    parameters = {
+      @Parameter(name = "serverId", description = "The id of the server to get the board on"),
+      @Parameter(name = "pointId", description = "The id of the point to get the board of"),
+      @Parameter(name = "timeStart", description = "The start time for the board, if omitted current server time will be used"),
+      @Parameter(name = "timeEnd", description = "The end time for the board, if omitted the start time plus 30 minutes will be used"),
+      @Parameter(name = "transportTypes", description = "Filter for transports that should be returned, if omitted all transports will be returned"),
+      @Parameter(name = "sortBy", description = "Sort order of board entries, of omitted entries will be sorted by realtime time information"),
+    },
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "The departure board entries were successfully resolved"),
+      @ApiResponse(
+        responseCode = "400",
+        description = "One of the given filter parameters is invalid or doesn't comply to the described constraints",
+        content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(
+        responseCode = "500",
+        description = "An internal error occurred while processing the request",
+        content = @Content(schema = @Schema(hidden = true))),
+    }
+  )
   public @Nonnull List<BoardEntryDto> listDepartures(
     @RequestParam(name = "serverId") @UUID(version = 5, allowNil = false) String serverId,
     @RequestParam(name = "pointId") @UUID(version = 4, allowNil = false) String pointId,
