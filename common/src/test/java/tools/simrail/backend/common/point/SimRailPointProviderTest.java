c345e718627167208f900758c6434de90a4273ec
Pasqual Koschmieder <git@derklaro.dev>
2025-04-25 21:39:10 +0200

    more new points

--- common/src/test/java/tools/simrail/backend/common/point/SimRailPointProviderTest.java
+++ common/src/test/java/tools/simrail/backend/common/point/SimRailPointProviderTest.java
@@ -63,7 +63,7 @@ public final class SimRailPointProviderTest {
   @Test
   void testPointsWereLoaded() {
     var points = this.pointProvider.points;
-    Assertions.assertEquals(603, points.size());
+    Assertions.assertEquals(614, points.size());
   }
 
   @Test
