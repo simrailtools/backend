c0919e5803fc1c08bc3488d349c47e1ab70d59e0
Pasqual Koschmieder <git@derklaro.dev>
2025-04-26 11:01:16 +0200

    feat: implement journey cancellation marking

--- info-collector/src/main/java/tools/simrail/backend/collector/journey/CollectorJourneyRepository.java
+++ info-collector/src/main/java/tools/simrail/backend/collector/journey/CollectorJourneyRepository.java
@@ -25,9 +25,13 @@
 package tools.simrail.backend.collector.journey;
 
 import jakarta.annotation.Nonnull;
+import java.time.OffsetDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tools.simrail.backend.common.journey.JourneyEntity;
 import tools.simrail.backend.common.journey.JourneyRepository;
 
@@ -44,4 +48,53 @@ interface CollectorJourneyRepository extends JourneyRepository {
 
   @Nonnull
   List<JourneyEntity> findAllByServerIdAndForeignRunIdIn(UUID serverId, Collection<UUID> foreignRunIds);
+
+  /**
+   * Finds all journeys on the specified server whose first playable event was not reached before the given time.
+   *
+   * @param serverTime the time of the server to check against.
+   * @param serverId   the id of the server to return the journeys of.
+   */
+  @Query(value = """
+    WITH first_playable_events AS (
+      SELECT
+        e.journey_id
+      FROM sit_journey_event e
+      WHERE
+        e.event_index = (
+          SELECT MIN(e2.event_index) + 1 -- select next event after after border to be absolutely sure
+          FROM sit_journey_event e2
+          WHERE e2.journey_id = e.journey_id AND e2.point_playable = TRUE
+        )
+        AND e.cancelled = false
+        AND e.scheduled_time < :time
+    )
+    SELECT j.id
+    FROM sit_journey j
+    JOIN first_playable_events fe ON fe.journey_id = j.id
+    WHERE
+      -- no need to select lastSeenTime here, but this way an index can be used more effectively
+      j.server_id = :serverId
+      AND j.first_seen_time IS NULL
+      AND j.last_seen_time IS NULL
+    """, nativeQuery = true)
+  List<UUID> findJourneysThatDidNotSpawn(@Param("time") OffsetDateTime serverTime, @Param("serverId") UUID serverId);
+
+  /**
+   * Marks the journeys with the given journey ids as canceled.
+   *
+   * @param journeyIds the ids of the journeys to mark as canceled.
+   */
+  @Modifying
+  @Query("UPDATE sit_journey j SET j.cancelled = TRUE WHERE j.id IN :journeyIds")
+  void markJourneysAsCancelled(@Param("journeyIds") Collection<UUID> journeyIds);
+
+  /**
+   * Marks the journey events associated with one of the given journeys as canceled.
+   *
+   * @param journeyIds the ids of the journeys whose events should be marked as canceled.
+   */
+  @Modifying
+  @Query("UPDATE sit_journey_event je SET je.cancelled = TRUE WHERE je.journeyId IN :journeyIds")
+  void markJourneyEventsAsCancelled(@Param("journeyIds") Collection<UUID> journeyIds);
 }
