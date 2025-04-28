5cacb81e8e976bf4a33a01f7edc1772a9d0419a7
Pasqual Koschmieder <git@derklaro.dev>
2025-04-28 20:36:19 +0200

    fix border points

--- common/src/test/java/tools/simrail/backend/common/border/MapBorderPointProviderTest.java
+++ common/src/test/java/tools/simrail/backend/common/border/MapBorderPointProviderTest.java
@@ -54,7 +54,7 @@ public final class MapBorderPointProviderTest {
   @Test
   void testAllBorderPointsWereLoaded() {
     var borderPoints = this.borderPointProvider.mapBorderPointIds;
-    Assertions.assertEquals(48, borderPoints.size());
+    Assertions.assertEquals(50, borderPoints.size());
   }
 
   @Test
