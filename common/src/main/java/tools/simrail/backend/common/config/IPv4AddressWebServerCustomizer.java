9a1540c2d227b11fe8670a8105ddf12f48e47563
derklaro <pasqual.koschmieder@easybill.de>
2025-06-16 09:37:12 +0200

    fix: force tomcat to use ipv4 wildcard address

---
+++ common/src/main/java/tools/simrail/backend/common/config/IPv4AddressWebServerCustomizer.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of simrail-tools-backend, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2024-2025 Pasqual Koschmieder and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package tools.simrail.backend.common.config;
+
+import jakarta.annotation.Nonnull;
+import java.net.Inet4Address;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Component to force the embedded web server to use the ipv4 wildcard address.
+ */
+@Component
+@Order(-10_000) // low priority to take high precedence
+public class IPv4AddressWebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void customize(@Nonnull ConfigurableServletWebServerFactory factory) {
+    var address = Inet4Address.ofLiteral("0.0.0.0");
+    factory.setAddress(address);
+  }
+}
