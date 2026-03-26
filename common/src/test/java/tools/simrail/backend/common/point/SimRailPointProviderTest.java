6ee6c432cf3f3718fdfd9efc008a3d6405594358
Pasqual Koschmieder <git@derklaro.dev>
2026-03-27 00:59:06 +0100

    chore: update base data for patch 2026-03-26

--- common/src/test/java/tools/simrail/backend/common/point/SimRailPointProviderTest.java
+++ common/src/test/java/tools/simrail/backend/common/point/SimRailPointProviderTest.java
@@ -39,7 +39,6 @@ import java.util.StringJoiner;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -126,7 +125,6 @@ public final class SimRailPointProviderTest {
   }
 
   @Test
-  @Disabled("Currently not correctly set in upstream api")
   void testMaxSpeedAtPointIsValid() {
     var points = this.pointProvider.points;
     for (var point : points) {
